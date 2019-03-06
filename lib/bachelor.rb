d915fc7e09db7c919e7d067b173da2f918e958e0
Annie Phan <anniephan.atp@gmail.com>
2019-03-06 21:13:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data.each do |season_number, contestant_info|
     if season_number.to_s == season && contestant_info[:status].to_s == "Winner"
-      return contestant_info[:name]
+      return contestant_info[:name].split(" ").first
     end
   end
 end
